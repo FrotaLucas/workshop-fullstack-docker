8b96365bbf88c3ecbb7a3a6947f41268ae60d1f0
FrotaLucas <lucasdiasfrota@gmail.com>
2025-04-02 19:56:57 -0300

    add empty constructor

--- backend/src/main/java/de/itestra/demo/DemoDTO.java
+++ backend/src/main/java/de/itestra/demo/DemoDTO.java
@@ -1,5 +1,7 @@
 package de.itestra.demo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.NotNull;
 
 public class DemoDTO {
@@ -13,13 +15,19 @@ public class DemoDTO {
         this.id = id;
         this.value = value;
     }
-
+    //emppty constructor to method toDto in DemoService
+    public DemoDTO(){
+    }
+    
+    // @JsonProperty("id") 
     public Long getId() {
         return id;
     }
     public void setId(Long id) {
         this.id = id;
     }
+
+    // @JsonProperty("value")
     public String getValue() {
         return value;
     }
